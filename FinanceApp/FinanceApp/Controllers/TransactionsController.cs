4f0a98c113a490323aa9b4fcb64139e328b4730c
Patrik Vogronics <vogronicsp@gmail.com>
2022-04-18 23:24:42 +0200

    extended transactions controller

--- FinanceApp/FinanceApp/Controllers/TransactionsController.cs
+++ FinanceApp/FinanceApp/Controllers/TransactionsController.cs
@@ -17,11 +17,32 @@ namespace FinanceApp.Controllers
             _transactionRepository = transactionRepository;
         }
 
-        // GET: api/<CardsController>
+        // GET: api/<TransactionsController>
         [HttpGet]
         public async Task<IEnumerable<Transaction>> GetTransactions()
         {
             return await _transactionRepository.GetAllTransactions();
+        }
+
+        // POST api/<TransactionsController>
+        [HttpPost]
+        public async Task<ActionResult<Transaction>> CreateTransaction([FromBody] Transaction transaction)
+        {
+            var newTransaction = await _transactionRepository.Create(transaction);
+            return CreatedAtAction(nameof(GetTransactions), new { id = newTransaction.Id }, newTransaction);
+        }
+
+        // DELETE api/<TransactionsController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var transactionToDelete = await _transactionRepository.GetTransactionById(id);
+            if (transactionToDelete == null)
+            {
+                return NotFound();
+            }
+            await _transactionRepository.Delete(transactionToDelete.Id);
+            return NoContent();
         }
 
     }
